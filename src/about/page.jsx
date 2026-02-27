0dca50b6b45d8d31147e2fb801916466753760dc
cautks <caua.ferreira.takasaki@gmail.com>
2026-02-27 14:38:23 -0300

    aula.2702

---
+++ src/about/page.jsx
@@ -0,0 +1,12 @@
+function Sobre() {
+    return(
+      <div>
+        <h1>Sobre</h1>
+        <p>Texto do meu lado, Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis adipisci nostrum, tenetur tempora aspernatur dolores. Iste eaque fugit delectus maiores nihil aspernatur officia laudantium eius. Voluptatibus suscipit a quidem praesentium.
+
+        </p>
+     </div>
+    )
+}
+
+export default Sobre;
