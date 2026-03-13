4d0119461fc95fa68c4ec859687901eb35cae06b
cautks <caua.ferreira.takasaki@gmail.com>
2026-03-13 14:40:16 -0300

    13032026

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,27 @@
-function Home () {
-  return(
-    <div>
-       <h1>aula de react com Vite JS</h1>
+import {useNavigate } from 'react-router-dom';
+import { Link } from 'react-router';
+function Home() {
 
+
+
+  const navigate = useNavigate();
+
+  return (
+    <div className='containerHome'>
+      <div className='containerCards'>
+      <h1>Aula de React com Vite JS</h1>
+
+      <button
+        onClick={() => navigate('/exemplo/1')}
+      >
+        Exemplo 1
+      </button>
+
+      <Link to="/exemplo/2">Exemplo 2</Link>
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default Home;
